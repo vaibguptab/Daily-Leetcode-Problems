5eeffb204b5d73c65f9b213a1c8c744694cf6695
Vaibhav Gupta <114448254+vaibguptab@users.noreply.github.com>
2023-03-10 10:05:36 +0530

    Update March_9th.java

--- March_9th.java
+++ March_9th.java
@@ -3,21 +3,21 @@ package Daily_Leetcode_Qs;
 public class March_9th {
     public static void main(String[] args) {
 
-//    }
-//    private static Node<Integer> Q142(Node<Integer> head) {
-//        Node<Integer> slow = head,fast = head,entry=head;
-//        while(fast!=null&&fast.next!=null){
-//            slow = slow.next;
-//            fast = fast.next.next;
-//            if (slow==fast){
-//                while (slow!=entry){
-//                    slow= slow.next;
-//                    entry = entry.next;
-//                }
-//                return entry;
-//            }
-//        }
-//
-//        return null;
+   }
+   private static Node<Integer> Q142(Node<Integer> head) {
+       Node<Integer> slow = head,fast = head,entry=head;
+       while(fast!=null&&fast.next!=null){
+           slow = slow.next;
+           fast = fast.next.next;
+           if (slow==fast){
+               while (slow!=entry){
+                   slow= slow.next;
+                   entry = entry.next;
+               }
+               return entry;
+           }
+       }
+
+       return null;
     }
 }
