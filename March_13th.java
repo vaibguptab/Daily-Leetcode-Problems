586a1ffe3fb4c6fd0c55bc1a6518b4e9e7b34b30
Vaibhav Gupta <114448254+vaibguptab@users.noreply.github.com>
2023-03-13 19:21:07 +0530

    Add files via upload

---
+++ March_13th.java
@@ -0,0 +1,15 @@
+package Daily_Leetcode_Qs;
+
+public class March_13th {
+    public static void main(String[] args) {
+
+    }
+    public boolean isSymmetric(TreeNode root) {
+        return root==null || isSymmetricHelp(root.left,root.right);
+    }
+    public boolean isSymmetricHelp(TreeNode left,TreeNode right){
+        if(left==null||right==null) return left==right;
+        if(left.val!=right.val) return false;
+        return isSymmetricHelp(left.left,right.right) && isSymmetricHelp(left.right,right.left);
+    }
+}
