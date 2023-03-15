56c19ac3be2f45a11e2d20c83467180efa4cb4a7
Vaibhav Gupta <114448254+vaibguptab@users.noreply.github.com>
2023-03-15 19:26:58 +0530

    Add files via upload

---
+++ March_14th.java
@@ -0,0 +1,22 @@
+package Daily_Leetcode_Qs;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class March_14th {
+    private static boolean CompleteBinaryTree(BinaryTreeNode<Integer> root) {
+        boolean end = false;
+        Queue<BinaryTreeNode<Integer>> queue = new LinkedList<>();
+        queue.add(root);
+        while(!queue.isEmpty()) {
+            BinaryTreeNode<Integer> cur = queue.poll();
+            if(cur == null) end = true;
+            else{
+                if(end) return false;
+                queue.add(cur.left);
+                queue.add(cur.right);
+            }
+        }
+        return true;
+    }
+}
