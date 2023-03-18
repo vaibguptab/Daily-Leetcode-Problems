762ad4955a89ee5da8090bd36d814b1cf189cfe0
Vaibhav Gupta <114448254+vaibguptab@users.noreply.github.com>
2023-03-18 22:42:40 +0530

    Add files via upload

---
+++ March_18th.java
@@ -0,0 +1,42 @@
+package Daily_Leetcode_Qs;
+
+class Node {
+
+    public Node prev;
+    public Node next;
+    public final String url;
+
+    public Node(final String url) {
+        this.url = url;
+    }
+}
+
+
+public class March_18th {
+    public March_18th(String homepage) {
+        curr = new Node(homepage);
+    }
+
+    public void visit(String url) {
+        curr.next = new Node(url);
+        curr.next.prev = curr;
+        curr = curr.next;
+    }
+
+    public String back(int steps) {
+        while(curr.prev!=null && steps-->0){
+            curr = curr.prev;
+        }
+        return curr.url;
+    }
+
+    public String forward(int steps) {
+        while(curr.next != null && steps-->0){
+            curr = curr.next;
+        }
+        return curr.url;
+    }
+
+    private Node curr;
+}
+
